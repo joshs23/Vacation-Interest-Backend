7dd66f1f49800b92a2fcac93198804a660889dda
joshs <josh.sommer23@gmail.com>
2024-08-20 19:18:58 -0700

    refactoreed to use pooling in mysql.connector to streamline connections

--- app/database.py
+++ app/database.py
@@ -1,44 +1,37 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import time
-import mysql.connector
+from mysql.connector import pooling
 from .config import settings
 
-def create_db_connection():
-    while True:
+# Set up the connection pool
+pool = pooling.MySQLConnectionPool(
+    pool_name="mypool",
+    pool_size=5,
+    user=settings.DB_USERNAME,
+    host=settings.DB_HOST,
+    password=settings.DB_PASSWORD,
+    database=settings.DB_NAME
+)
 
-        try:
-            cnx = mysql.connector.connect(user=settings.DB_USERNAME, 
-                                        host=settings.DB_HOST, 
-                                        password=settings.DB_PASSWORD, 
-                                        database=settings.DB_NAME)
-            print(("DATABASE: Connection success"))
-            return cnx
-            break
-        except Exception as error: 
-            print("connection failure")
-            print("Error: ", error)
-            time.sleep(5)
-
-SQLALCHEMY_DATABASE_URL = ("mysql+mysqlconnector://%s:%s@%s/%s" % (settings.DB_USERNAME, 
-                                                                   settings.DB_PASSWORD, 
-                                                                   settings.DB_HOST, 
-                                                                   settings.DB_NAME))
+# SQLAlchemy setup
+SQLALCHEMY_DATABASE_URL = (
+    "mysql+mysqlconnector://%s:%s@%s/%s" % 
+    (settings.DB_USERNAME, settings.DB_PASSWORD, settings.DB_HOST, settings.DB_NAME)
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
 def get_cursor():
-    cnx = create_db_connection()
+    cnx = pool.get_connection()
     cursor = cnx.cursor(buffered=True)
     try:
         yield cursor, cnx
