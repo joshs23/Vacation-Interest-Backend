487234bc4680e5522a00c236a452a513e6e7afef
joshs <josh.sommer23@gmail.com>
2024-09-29 16:59:56 -0700

    made check owner available for all tables

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from fastapi import status, HTTPException
+import mysql.connector
+from mysql.connector import errorcode
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +10,27 @@ def hash(password: str):
 
 def verify(plain_pwd: str, hashed_pwd: str):
     return pwd_context.verify(plain_pwd, hashed_pwd)
+
+### Helper function to check group owner for updates and deletes
+def checkOwner(group_id, current_user_id, cursor, table):
+    isOwner = False
+    try:
+        query = f"""SELECT Owner_id
+                          FROM {table}
+                          WHERE Group_id = %s"""
+        cursor.execute(query, (group_id, ))
+        owner = cursor.fetchone()
+        if owner is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Group not found.")
+        if(owner[0] == current_user_id):
+            isOwner = True
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_DUP_ENTRY:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"Group not found.")
+        else:
+            print(err)
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail="Internal server error occurred.")
+    return isOwner
